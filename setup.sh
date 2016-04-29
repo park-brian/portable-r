a082af55bc273e0499c9f03c0b51449918c8d140
park-brian <re.public@outlook.com>
2016-04-29 10:05:48 -0400

    added setup file to initialize R

---
+++ setup.sh
@@ -0,0 +1,24 @@
+
+rm -rf .tmp && mkdir .tmp && cd .tmp
+
+# Download R 3.2.5 for Windows
+echo $'\n-- Downloading R 3.2.5 for Windows (https://cran.r-project.org/bin/windows/base/R-3.2.5-win.exe) --\n'
+curl -O https://cran.r-project.org/bin/windows/base/R-3.2.5-win.exe
+
+# Download InnoExtract (https://github.com/dscharrer/innoextract)
+echo $'\n-- Downloading InnoExtract (https://github.com/dscharrer/innoextract/releases/download/1.6/innoextract-1.6-windows.zip) --\n'
+curl -O -L https://github.com/dscharrer/innoextract/releases/download/1.6/innoextract-1.6-windows.zip
+
+echo $'\n-- Unzipping InnoExtract --\n'
+unzip innoextract-1.6-windows.zip
+
+echo $'\n-- Running InnoExtract on downloaded archive --\n'
+winpty innoextract.exe -d ../ R-3.2.5-win.exe
+
+echo $'\n-- Removing .tmp directory --\n'
+cd .. && rm -rf .tmp
+
+echo $'\n-- Setting portable R library path --\n'
+echo ".First = function() { .libPaths(.Library) } >> app/etc/Rprofile.site
+
+
